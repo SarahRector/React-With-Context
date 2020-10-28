d9925551fcc962d359a79bcb38be2ac858484b31
SarahRector <rector.sarah.r@gmail.com>
2020-10-28 16:14:31 -0700

    added header and character page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CharacterPage from '../../pages/CharacterPage';
+import Header from '../header/Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={CharacterPage} />
+      </Switch>
+    </Router>
+  );
 }
